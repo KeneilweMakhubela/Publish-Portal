40d4c21617c1ec27b982d398546d45bf31ecc955
KeneilweMakhubela <keneilwe.j.makhubela@gmail.com>
2025-08-07 21:09:50 +0200

    Init

---
+++ API/Online-Store/Online-Store/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Store.Models.Domain;
+
+namespace Online_Store.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPostC { get; set; }
+        public DbSet<Category> CategoryC  { get; set; }
+    }
+}
---
+++ API/Online-Store/Online-Store/Repository/Interface/ICreateCatRepo.cs
@@ -0,0 +1,23 @@
+﻿using Online_Store.Models.Domain;
+
+namespace Online_Store.Repository.Interface
+{
+    public interface ICreateCatRepo
+    {
+        //Take a category insert to a db and return a created category
+        Task<Category>CreateAsync(Category category);
+        
+        Task<Category?> GetByIdAsync(Guid id);
+
+
+        Task<IEnumerable<Category>> GetAllAsync();
+
+        Task<Category?> UpdateAsync(Guid id, Category category);
+
+        Task<bool> DeleteAsync(Guid id);
+
+        Task DeleteAllAsync();
+
+
+    }
+}
