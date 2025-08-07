d5402c921f72f8bdab182df6cf2262212dd00390
KeneilweMakhubela <51229431+KeneilweMakhubela@users.noreply.github.com>
2025-08-07 22:47:22 +0200

    Delete API/Online-Store directory

--- API/Online-Store/Online-Store/Models/DTO/CatRespDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Online_Store.Models.DTO
-{
-    public class CatRespDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string UrlHandle { get; set; }
-    }
-}
--- API/Online-Store/Online-Store/Models/DTO/CreateCatReqDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Online_Store.Models.DTO
-{
-    public class CreateCatReqDto
-    {
-        public string Name { get; set; }
-        public string UrlHandle { get; set; }
-    }
-}
