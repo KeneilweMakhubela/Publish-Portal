e7cd38c11c5b3a40f760c032cd64305469f2848f
KeneilweMakhubela <keneilwe.j.makhubela@gmail.com>
2025-08-07 23:20:57 +0200

    Init

---
+++ API/Online-Store/Online-Store/Models/DTO/CatRespDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Store.Models.DTO
+{
+    public class CatRespDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
